96a3ad4577139498971b1b2288c1f445cf967b51
Hb-zzZ <Hb-email@foxmail.com>
2020-08-31 15:42:56 +0800

    📝 Docs: 更改文档打包base路径

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,7 +3,7 @@ const themeConfig = require('./config/')
 module.exports = {
   theme: 'reco',
   //   title: 'gen-vue-form',
-  base: '/docs/',
+  base: '/',
   dest: './docs',
   description: 'gen-vue-form 文档，方便你更快入手！',
   head: [
